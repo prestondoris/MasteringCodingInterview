c6494f0c0da033fff468a96e70acb4a1fb2b7d4b
Preston Doris <prestondoris@gmail.com>
2019-09-05 08:11:53 -0700

    Section 13 Sorting

---
+++ Section13/BubbleSort.js
@@ -0,0 +1,17 @@
+function bubbleSort(arr) {
+  const len = arr.length;
+
+  for(let i=0; i<len; i++) {
+    for(let j=0; j<len; j++) {
+      if(arr[j] > arr[j+1]) {
+        let temp=arr[j];
+        arr[j] = arr[j+1]
+        arr[j+1] = temp;
+      }
+    }
+  }
+}
+
+const array = [99,44,6,2,1,5,63,87,283,4,0]
+bubbleSort(array);
+console.log(array);
